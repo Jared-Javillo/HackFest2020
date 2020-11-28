1e5d9d7d084207bbfbc4ce585975d724f5792b49
avsecam <avisucam@gmail.com>
2020-11-28 15:15:49 +0800

    fixed widget_test import, removed firebase

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:HackFest2020/main.dart';
+import 'package:Akay/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
