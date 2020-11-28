4fdf15f7d49dffd3a5164eadacfb0c8e9728249c
Terariver <jared.javillo@obf.ateneo.edu>
2020-11-28 16:50:53 +0800

    Added lists for mentors and mentees

---
+++ lib/MentorList.dart
@@ -0,0 +1,13 @@
+import './FindMentorWidget.dart';
+
+List<FindMentorWidget> mentees = [
+  FindMentorWidget("Monika", "@ddlcmonika", "Mathematics", "I like Anime UwU",
+      "All monika <monkaS>", "assets/images/monika.png"),
+  FindMentorWidget(
+      "Sayori",
+      "@notThePuppet",
+      "Minecraft Speedrunning",
+      "I'm not the puppet from Naruto",
+      "Pokimane Queen",
+      "assets/images/sayori.png"),
+];
