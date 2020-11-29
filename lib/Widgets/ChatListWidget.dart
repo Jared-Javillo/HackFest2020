706499aefcc473b87edc89c66532ded2fc5febd9
Terariver <jared.javillo@obf.ateneo.edu>
2020-11-29 10:25:42 +0800

    added dividers in chatlist

--- lib/Widgets/ChatListWidget.dart
+++ lib/Widgets/ChatListWidget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 import '../ModelsAndLists/MessageModel.dart';
 
 class ChatListWidget extends StatelessWidget {
@@ -6,6 +8,7 @@ class ChatListWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
+        SizedBox(height: ScreenUtil().setHeight(100)),
         Row(
           children: [
             Container(
@@ -39,78 +42,91 @@ class ChatListWidget extends StatelessWidget {
           itemCount: chats.length,
           itemBuilder: (BuildContext context, int index) {
             final Message chat = chats[index];
-            return Container(
-              margin: EdgeInsets.only(bottom: 5.0, top: 5.0, right: 20.0),
-              padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Row(
+            return Column(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(bottom: 5.0, top: 5.0, right: 20.0),
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
-                      CircleAvatar(
-                        radius: 35.0,
-                        backgroundImage: AssetImage(chat.sender.imageUrl),
+                      Row(
+                        children: <Widget>[
+                          CircleAvatar(
+                            radius: 35.0,
+                            backgroundImage: AssetImage(chat.sender.imageUrl),
+                          ),
+                          SizedBox(width: 10.0),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                chat.sender.name,
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Container(
+                                width: MediaQuery.of(context).size.width * 0.45,
+                                child: Text(
+                                  chat.text,
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 15.0,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
-                      SizedBox(width: 10.0),
                       Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
+                        children: <Widget>[
                           Text(
-                            chat.sender.name,
+                            chat.time,
                             style: TextStyle(
                               color: Colors.grey,
                               fontSize: 15.0,
                               fontWeight: FontWeight.bold,
                             ),
                           ),
-                          Container(
-                            width: MediaQuery.of(context).size.width * 0.45,
-                            child: Text(
-                              chat.text,
-                              style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 15.0,
-                                fontWeight: FontWeight.w500,
-                              ),
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ),
+                          SizedBox(height: 4.0),
+                          chat.unread
+                              ? Container(
+                                  width: 40.0,
+                                  height: 20.0,
+                                  decoration: BoxDecoration(
+                                    color: Colors.blueGrey,
+                                    borderRadius: BorderRadius.circular(30.0),
+                                  ),
+                                  alignment: Alignment.center,
+                                  child: Text('UNREAD',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 8.0,
+                                        fontWeight: FontWeight.bold,
+                                      )),
+                                )
+                              : Text(''),
                         ],
                       ),
                     ],
                   ),
-                  Column(
-                    children: <Widget>[
-                      Text(
-                        chat.time,
-                        style: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 15.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: 4.0),
-                      chat.unread
-                          ? Container(
-                              width: 40.0,
-                              height: 20.0,
-                              decoration: BoxDecoration(
-                                color: Colors.blueGrey,
-                                borderRadius: BorderRadius.circular(30.0),
-                              ),
-                              alignment: Alignment.center,
-                              child: Text('UNREAD',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 8.0,
-                                    fontWeight: FontWeight.bold,
-                                  )),
-                            )
-                          : Text(''),
-                    ],
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                      bottom: 1, top: 0, left: 50.0, right: 40.0),
+                  child: Divider(
+                    color: Colors.white,
+                    thickness: 2,
                   ),
-                ],
-              ),
+                )
+              ],
             );
           },
         ))
